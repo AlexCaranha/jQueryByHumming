bedb72e958619f968f9cf66d8b237c270056fcc3
alexcaranha <alexcaranha@alexcaranha-teste>
2013-11-06 22:03:19 -0200

    Avanço no levantamento estatístico.

--- src/main/java/com/alexcaranha/jquerybyhumming/EvaluationSystem.java
+++ src/main/java/com/alexcaranha/jquerybyhumming/EvaluationSystem.java
@@ -213,6 +213,64 @@ public class EvaluationSystem {
         return null;
     }
     
+    // PAREI AQUI.
+    public static String getCode(String label) {
+        if (label.contains("musica_002_")) return "Águas de março";
+        if (label.contains("musica_003_")) return "Carinhoso";
+        if (label.contains("musica_004_")) return "Asa branca";
+        if (label.contains("musica_006_")) return "Chega de saudade";
+        if (label.contains("musica_008_")) return "Detalhes";
+        if (label.contains("musica_010_")) return "Alegria, alegria";
+        if (label.contains("musica_012_")) return "Aquarela do Brasil";
+        if (label.contains("musica_015_")) return "Trem das onze";
+        if (label.contains("musica_019_")) return "Quero que vá tudo pro inferno";
+        if (label.contains("musica_020_")) return "Preta pretinha";
+        if (label.contains("musica_023_")) return "Inútil";
+        if (label.contains("musica_024_")) return "Eu sei que vou te amar";
+        if (label.contains("musica_025_")) return "País tropical";
+        if (label.contains("musica_027_")) return "Garota de ipanema";
+        if (label.contains("musica_028_")) return "Pra não dizer que não falei das flores";
+        if (label.contains("musica_031_")) return "Travessia";
+        if (label.contains("musica_038_")) return "Eu quero é botar meu bloco na rua";
+        if (label.contains("musica_041_")) return "Manhã de carnaval";
+        if (label.contains("musica_046_")) return "Ponteio";
+        if (label.contains("musica_047_")) return "Me chama";
+        if (label.contains("musica_057_")) return "Conversa de botequim";
+        if (label.contains("musica_062_")) return "Luar do sertão";
+        if (label.contains("musica_063_")) return "Alagados";
+        if (label.contains("musica_064_")) return "As curvas da estrada de Santos";
+        if (label.contains("musica_067_")) return "A banda";
+        if (label.contains("musica_068_")) return "Comida";
+        if (label.contains("musica_070_")) return "Ronda";
+        if (label.contains("musica_072_")) return "Gita";
+        if (label.contains("musica_074_")) return "Sentado à beira do caminho";
+        if (label.contains("musica_075_")) return "Foi um rio que passou em minha vida";
+        if (label.contains("musica_081_")) return "Que país é este?";
+        if (label.contains("musica_083_")) return "Ideologia";
+        if (label.contains("musica_084_")) return "Rosa";
+        if (label.contains("musica_085_")) return "O barquinho";
+        if (label.contains("musica_087_")) return "Meu mundo e nada mais";
+        if (label.contains("musica_089_")) return "A flor e o espinho";
+        if (label.contains("musica_091_")) return "Felicidade";
+        if (label.contains("musica_093_")) return "Casa no campo";
+        if (label.contains("musica_096_")) return "Disritmia";
+        if (label.contains("musica_097_")) return "Você não soube me amar";
+        if (label.contains("musica_098_")) return "A noite de meu bem";
+        if (label.contains("musica_100_")) return "Anna Júlia";
+        if (label.contains("musica_101_")) return "Parabéns a você";
+        if (label.contains("musica_102_")) return "Ciranda cirandinha";
+        if (label.contains("musica_103_")) return "Escravos de jó";
+        if (label.contains("musica_104_")) return "Hino nacional";
+        if (label.contains("musica_105_")) return "Hino da bandeira";
+        if (label.contains("musica_107_")) return "Ilariê";
+        if (label.contains("musica_108_")) return "Fim de Ano";
+        if (label.contains("musica_109_")) return "Mamãe eu quero";
+        if (label.contains("musica_111_")) return "Coelhinho de olhos vermelhos";
+        if (label.contains("musica_112_")) return "Chegou a hora da foqueira";
+        
+        return null;
+    }
+    
     public static void main(String[] args) throws FileNotFoundException, IOException {
         String dirDB = "/home/alexcaranha/Documentos/Mestrado/DataBase/DBSolfejos";
         
@@ -228,7 +286,7 @@ public class EvaluationSystem {
         List<String> titles = getTitles();        
         List<String> tipos = Util.createList("Tipo1","Tipo2","Tipo3");
                 
-        FileWriter outFile = new FileWriter(Util.getDirExecution("DadosBD.txt"));
+        FileWriter outFile = new FileWriter(Util.getDirExecution("DadosEstatisticosDB.txt"));
         PrintWriter out = new PrintWriter(outFile);
         
         Map<Integer, List<Usuario>> usuarios = new HashMap<Integer, List<Usuario>>();
@@ -238,7 +296,7 @@ public class EvaluationSystem {
         usuarios.put(2, usuarios2);  // DTW_RelativePitch
         usuarios.put(3, usuarios3);  // DTW_AbsolutePitch
         //---------------------------------------------------------------------------------------------
-        System.out.println("Gravações por música: ");
+        System.out.println("Gravações por música: Codigo;Titulo;Tipo1;Tipo2;Tipo3");
         out.println("Gravações por música: Codigo;Titulo;Tipo1;Tipo2;Tipo3");
 
         File raiz = new File(dirDB);
@@ -273,13 +331,14 @@ public class EvaluationSystem {
                 }
             }
 
-            codigoMusica = codigoMusica.replaceAll("musica_", "").replaceAll("_g1a", "").replaceAll("_g1b", "").replaceAll("_g2", "").replaceAll("_s", "");
+            codigoMusica = codigoMusica.replaceAll("musica_", "").replaceAll("_g1a", "").replaceAll("_g1b", "").replaceAll("_g2", "").replaceAll("_s", "").replaceAll(".wav", "");
             System.out.println(String.format("%s;%s;%d;%d;%d",codigoMusica, title, qtdTipo1, qtdTipo2, qtdTipo3));            
             out.println(String.format("%s;%s;%d;%d;%d",codigoMusica, title, qtdTipo1, qtdTipo2, qtdTipo3));            
         }
-        //---------------------------------------------------------------------------------------------
-        System.out.println("Gravações por música: ");
-        out.println("Gravações saturadas por música: Codigo;Titulo;Tipo1;Tipo2;Tipo3");
+        
+        //----------------------------------------------------------------------
+        System.out.println("\nGravações saturadas por música: Codigo;Titulo;Tipo1;Tipo2;Tipo3");
+        out.println("\nGravações saturadas por música: Codigo;Titulo;Tipo1;Tipo2;Tipo3");
 
         File diretorio = new File(dirDB);
         for(String title : titles) {
@@ -297,7 +356,7 @@ public class EvaluationSystem {
                         String tipo = fileNivel2.getName();
                         if ("Tipo1|Tipo2|Tipo3".contains(tipo)) {
                             for(File fileNivel3: fileNivel2.listFiles()) {
-                                if (fileNivel3.getName().contains(".wav") && fileNivel3.getName().contains("s")) {
+                                if (fileNivel3.getName().contains(".wav") && fileNivel3.getName().contains("_s.wav")) {
                                     String musicaEsperada = getTitle(fileNivel3.getName().trim());
                                     if (musicaEsperada.equalsIgnoreCase(title)) {
                                         codigoMusica = fileNivel3.getName().trim();
@@ -313,14 +372,14 @@ public class EvaluationSystem {
                 }
             }
 
-            codigoMusica = codigoMusica.replaceAll("musica_", "").replaceAll("_g1a", "").replaceAll("_g1b", "").replaceAll("_g2", "").replaceAll("_s", "");
+            codigoMusica = codigoMusica.replaceAll("musica_", "").replaceAll("_g1a", "").replaceAll("_g1b", "").replaceAll("_g2", "").replaceAll("_s", "").replaceAll(".wav", "");
             System.out.println(String.format("%s;%s;%d;%d;%d",codigoMusica, title, qtdTipo1, qtdTipo2, qtdTipo3));            
             out.println(String.format("%s;%s;%d;%d;%d",codigoMusica, title, qtdTipo1, qtdTipo2, qtdTipo3));            
         }
         //---------------------------------------------------------------------------------------------
         // Tipo -> Algoritmo -> MRR (usuarios).
-        System.out.println("MRR por tipo de gravação e algoritmo: Tipo; MRR Algoritmo1; MRR Algoritmo2; MRR Algoritmo3");
-        out.println("MRR por tipo de gravação e algoritmo: Tipo; MRR Algoritmo1; MRR Algoritmo2; MRR Algoritmo3");
+        System.out.println("\nMRR por tipo de gravação e algoritmo: Tipo; MRR Algoritmo1; MRR Algoritmo2; MRR Algoritmo3");
+        out.println("\nMRR por tipo de gravação e algoritmo: Tipo; MRR Algoritmo1; MRR Algoritmo2; MRR Algoritmo3");
                 
         for(String tipo : tipos) {
             int qtdAlgoritmo1 = 0;
@@ -334,47 +393,34 @@ public class EvaluationSystem {
             for(Entry<Integer, List<Usuario>> item : usuarios.entrySet()) {
                 Integer       algoritmo = item.getKey();
                 List<Usuario> lista     = item.getValue();
-
-                // PAREI AQUI.
-                
+                                
                 for(Usuario usuario : lista) {
-                    for(Gravacao gravacao : usuario.getGravacoes()) {
-                        KeyValue<Integer, Double> parMRR = MRR_Algoritmo.get(gravacao.getTipo());
-                        
-                        int     qtdLast = parMRR.getKey();
-                        double  somaLast = parMRR.getValue();
-                        
-                        int     qtdNew = 1 + qtdLast;
-                        double  somaNew = (1 / gravacao.getPosicao()) + somaLast;
-                        
-                        parMRR.setKey(qtdNew);
-                        parMRR.setValue(somaNew);
+                    for(Gravacao gravacao : usuario.getGravacoes()) {                        
+                        if (gravacao.getTipo().equalsIgnoreCase(tipo)) {
+                            switch (algoritmo) {
+                                case 1:
+                                    qtdAlgoritmo1 += 1;
+                                    sumAlgoritmo1 += 1 / gravacao.getPosicao();
+                                    break;
+                                case 2:
+                                    qtdAlgoritmo2 += 1;
+                                    sumAlgoritmo2 += 1 / gravacao.getPosicao();
+                                    break;
+                                case 3:
+                                    qtdAlgoritmo3 += 1;
+                                    sumAlgoritmo3 += 1 / gravacao.getPosicao();
+                                    break;
+                            }
+                        }
                     }
                 }
-
-                
-                
-                System.out.println(String.format("%s;%s;%d;%d;%d",codigoMusica, title, qtdTipo1, qtdTipo2, qtdTipo3));            
             }
-        }
-        //---------------------------------------------------------------------------------------------
-        System.out.println("MRR por tipo de gravação e algoritmo: MRR Algoritmo1;MRR Algoritmo2;MRR Algoritmo3;Titulo");
-        out.println("MRR por tipo de gravação e algoritmo: MRR Algoritmo1;MRR Algoritmo2;MRR Algoritmo3;Titulo");
+            
+            double MRR_A1 = qtdAlgoritmo1 == 0 ? 0.0 : sumAlgoritmo1 / qtdAlgoritmo1;
+            double MRR_A2 = qtdAlgoritmo2 == 0 ? 0.0 : sumAlgoritmo2 / qtdAlgoritmo2;
+            double MRR_A3 = qtdAlgoritmo3 == 0 ? 0.0 : sumAlgoritmo3 / qtdAlgoritmo3;
 
-        for(String title : titles) {            
-            KeyValue<Integer, Double> MRR_Algoritmo1 = new KeyValue<Integer, Double>(0, 0.0);
-            KeyValue<Integer, Double> MRR_Algoritmo2 = new KeyValue<Integer, Double>(0, 0.0);
-            KeyValue<Integer, Double> MRR_Algoritmo3 = new KeyValue<Integer, Double>(0, 0.0);
-                        
-            for(Entry<Integer, List<Usuario>> item : usuarios.entrySet()) {
-                Integer         algoritmo = item.getKey();
-                List<Usuario>   lista = item.getValue();
-                
-                for(Usuario usuario : lista) {
-                    List<Gravacao> gravacaoTitle = usuario.getGravacao(title);
-                    
-                }
-            }
+            System.out.println(String.format("%s;%.6fs;%.6f;%.6f", tipo, MRR_A1, MRR_A2, MRR_A3));
         }
         //---------------------------------------------------------------------------------------------        
         out.close();
